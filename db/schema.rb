37125132086b8274fe81e1bed9b160a1ba6bca6c
Auriga <area51alienware@gmail.com>
2017-01-21 09:41:35 +0000

    Added image upload to new and controlllaarrr

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170121063909) do
+ActiveRecord::Schema.define(version: 20170121092814) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "desc"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
