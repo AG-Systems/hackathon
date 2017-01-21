f001e9f7278ad7699fd4ee4652c6bf67356eeffb
Auriga <area51alienware@gmail.com>
2017-01-21 09:03:57 +0000

    Added post new food method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,22 @@
 class PostsController < ApplicationController
+    before_filter :authenticate_user!, :only => [:new]
+    include PostsHelper
     def index
         @posts = Post.all
+    end
+    
+    def show
+    end
+    
+    def new
+        @post = Post.new
+    end
+    
+    def create
+        @post = Post.new(post_params)
+        @post.save
+    
+        # redirect_to job_path(@job)
+        redirect_to root_path
     end
 end
